799a79a7fdc2f95f21a0f193b7d23d7c2f94aff8
Toktobekov Aidar <aidar.omurbekov05@gmail.com>
2024-09-10 09:40:43 +0600

    Добавил middleware auth.ts, добавил роут user.ts

---
+++ todo-api/index.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import cors from 'cors';
+import config from './config';
+import userRouter from './roters/user';
+import mongoose from 'mongoose';
+
+const app = express();
+const port = 8000;
+
+app.use(cors(config.corsOptions));
+app.use(express.json());
+app.use(express.static("pablic"));
+app.use('/user', userRouter);
+
+const run = async()=>{
+    await mongoose.connect(config.database);
+
+    app.listen(port, ()=>{
+        console.log(`Server started on ${port} port!`);
+    });
+
+    process.on('exit', ()=>{
+        mongoose.disconnect();
+    });
+};
+
+run().catch(console.error);
